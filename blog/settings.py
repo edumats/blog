37b27ffbf10bcc8f435518fcb9426b02d1ed45f7
edumats <eduardo.mats@gmail.com>
2021-01-15 13:27:47 -0300

    Heroku db settings

--- blog/settings.py
+++ blog/settings.py
@@ -202,6 +202,6 @@ TINYMCE_DEFAULT_CONFIG = {
     }
 
 # Heroku: Update database configuration from $DATABASE_URL.
-""" import dj_database_url
+import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env) """
+DATABASES['default'].update(db_from_env)
